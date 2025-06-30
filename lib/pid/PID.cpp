b8fb585e12407290cf173a4846673afd787fb272
Zune-Z3 <annamrajun1@gmail.com>
2025-06-29 22:45:03 -0400

    PID::Calculate() now clamps output with min/max; added PID::Reset()

--- lib/pid/PID.cpp
+++ lib/pid/PID.cpp
@@ -2,6 +2,7 @@
 // Created by Nikhil Annamraju on 6/28/2025.
 //
 
+#include <algorithm>
 #include "PID.h"
 
 PID::PID(double kp, double ki, double kd, double min, double max):
@@ -17,8 +18,14 @@ double PID::Calculate(double setPoint, double processVar, double dt) {
     double DOut = Kd * (Err - LastErr) / dt;
 
     double Output = POut + IOut + DOut;
+    Output = std::max(Min, std::min(Output, Max));
 
     LastErr = Err;
     return Output;
+}
+
+void PID::Reset() {
+    LastErr = 0;
+    IntSum = 0;
 }
 
