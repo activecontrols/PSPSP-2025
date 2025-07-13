a13094c21035db694d276a7c7b8422112a65d257
Zune-Z3 <annamrajun1@gmail.com>
2025-07-13 19:42:26 -0400

    PID object now calculates dt based on a LastTime duration stored in the class

--- lib/pid/PID.cpp
+++ lib/pid/PID.cpp
@@ -6,10 +6,18 @@
 #include "PID.h"
 
 PID::PID(double kp, double ki, double kd, double min, double max):
-    Kp(kp), Ki(ki), Kd(kd), Min(min), Max(max), LastErr(0), IntSum(0)
+    Kp(kp), Ki(ki), Kd(kd), Min(min), Max(max), LastErr(0), IntSum(0), LastTime(std::chrono::milliseconds(-1))
 {}
 
-double PID::Calculate(double setPoint, double processVar, double dt) {
+double PID::Calculate(double setPoint, double processVar) {
+    auto now = std::chrono::high_resolution_clock::now();
+    auto epoch = std::chrono::duration<double, std::milli>(now.time_since_epoch());
+
+    if (LastTime.count() == -1) {
+        LastTime = epoch;
+    }
+
+    double dt = (epoch.count() - LastTime.count()) / 1000;
     double Err = setPoint - processVar;
     IntSum += Err * dt;
 
@@ -17,15 +25,21 @@ double PID::Calculate(double setPoint, double processVar, double dt) {
     double IOut = Ki * IntSum;
     double DOut = Kd * (Err - LastErr) / dt;
 
+    if (dt == 0) {
+        DOut = 0;
+    }
+
     double Output = POut + IOut + DOut;
     Output = std::max(Min, std::min(Output, Max));
 
     LastErr = Err;
+    LastTime = epoch;
     return Output;
 }
 
 void PID::Reset() {
     LastErr = 0;
     IntSum = 0;
+    LastTime = std::chrono::milliseconds(-1);
 }
 
