9bae802cdcb6c7abd6151d7284e92182ef3a3298
Zune-Z3 <annamrajun1@gmail.com>
2025-07-13 19:42:50 -0400

    Updated class definition to implement duration LastTime

--- lib/pid/PID.h
+++ lib/pid/PID.h
@@ -8,14 +8,17 @@
 #ifndef PID_CONTROLLER
 #define PID_CONTROLLER
 
+#include <chrono>
+
 class PID {
     public:
         PID(double kp, double ki, double kd, double min, double max);
-        double Calculate(double setPoint, double processVar, double dt);
+        double Calculate(double setPoint, double processVar);
         void Reset();
     private:
         double Kp, Ki, Kd, Min, Max;
         double LastErr, IntSum;
+        std::chrono::duration<double, std::milli> LastTime;
 };
 
 #endif
